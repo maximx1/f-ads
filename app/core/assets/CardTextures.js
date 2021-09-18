6b6e2dd9778481d92c14bc7359fd0ff5da29a97f
Justin Walrath <walrathjaw@gmail.com>
2021-09-18 00:31:59 -0400

    [#3] Added Ace of diamonds clubs and hearts. Added a texture loading scheme that directly loads svgs into an object to pass to ResourceManager

---
+++ app/core/assets/CardTextures.js
@@ -0,0 +1,13 @@
+import cardBack from './cardback.svg';
+import aceOfSpades from './ace_of_spades.svg';
+import aceOfClubs from './ace_of_clubs.svg';
+import aceOfHearts from './ace_of_hearts.svg';
+import aceOfDiamonds from './ace_of_diamonds.svg';
+
+export default {
+    cardBack,
+    aceOfSpades,
+    aceOfClubs,
+    aceOfHearts,
+    aceOfDiamonds
+}
--- app/core/gameutils/ResourceManager.js
+++ app/core/gameutils/ResourceManager.js
@@ -1,6 +1,5 @@
 import { Loader, Texture } from 'excalibur';
-import cardBackTexture from '../assets/cardback.svg';
-import aceOfSpades from '../assets/ace_of_spades.svg';
+import cardTextures from '../assets/CardTextures';
 
 export default class ResourceManager {
     constructor() {
@@ -13,10 +12,17 @@ export default class ResourceManager {
     }
 
     setUpTexturesForLoad() {
-        this.resources['cardBack'] = new Texture(cardBackTexture);
-        this.loader.addResource(this.resources.cardBack);
+        for (let [key, value] of Object.entries(cardTextures)) {
+            this.resources[key] = new Texture(value);
+            this.loader.addResource(this.resources[key]);
+        }
+        
 
-        this.resources['aceOfSpades'] = new Texture(aceOfSpades);
-        this.loader.addResource(this.resources.aceOfSpades);
+
+        // this.resources['cardBack'] = new Texture(cardBackTexture);
+        // this.loader.addResource(this.resources.cardBack);
+
+        // this.resources['aceOfSpades'] = new Texture(aceOfSpades);
+        // this.loader.addResource(this.resources.aceOfSpades);
     }
 }
